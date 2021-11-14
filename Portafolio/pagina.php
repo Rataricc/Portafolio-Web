7da06d231863119b678dab2d29f85d411c1efc86
Lautaro Rataric <lautarorataric163@gmail.com>
2021-11-14 01:22:49 -0300

    modifique nuevamente archivo php

--- Portafolio/pagina.php
+++ Portafolio/pagina.php
@@ -3,7 +3,7 @@ $nombre = $_POST['nombre'];
 $email = $_POST['email']; 
 $mensaje = $_POST['message']; 
 
-mail('ratariclautaro8@gmail.com', $email, $message); 
+mail('ratariclautaro8@gmail.com', $email, $message, $header; 
 
 header("Location:index.html"); 
 
